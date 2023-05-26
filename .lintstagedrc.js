e7cb957a0ea25631d54840b76eef40ef75c28276
eechih <eechih@gmail.com>
2023-05-26 11:33:28 +0800

    setup eslint, prettier, husky & lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // Type check TypeScript files
+  '**/*.(ts|tsx)': () => 'pnpm tsc --noEmit',
+
+  // Lint & Prettify TS and JS files
+  '**/*.(ts|tsx|js)': filenames => {
+    return [
+      `pnpm eslint ${filenames.join(' ')}`,
+      `pnpm prettier --write ${filenames.join(' ')}`
+    ]
+  },
+
+  // Prettify only Markdown and JSON files
+  '**/*.(md|json)': filenames => `pnpm prettier --write ${filenames.join(' ')}`
+}
